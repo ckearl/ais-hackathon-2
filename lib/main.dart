272bac078e5aa4d2c4751d4029f4cb140290f005
jackestes06 <jackestes3210@gmail.com>
2024-02-29 18:01:50 -0700

    Buttons setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:social_login_buttons/social_login_buttons.dart';
 
 import 'home_page.dart';
 
@@ -67,6 +68,160 @@ class MicrosoftLoginWidget extends StatefulWidget {
 }
 
 class _MicrosoftLoginWidgetState extends State<MicrosoftLoginWidget> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  String errorMessage = "";
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return SingleChildScrollView(
+        padding: const EdgeInsets.all(6),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40),
+                Container(
+                  constraints: BoxConstraints(
+                    minWidth: 500,
+                    maxWidth: constraints.maxWidth,
+                  ),
+                  child: FractionallySizedBox(
+                    widthFactor: .5,
+                    child: TextField(
+                      controller: emailController,
+                      cursorColor: Colors.black,
+                      textInputAction: TextInputAction.next,
+                      decoration: const InputDecoration(labelText: "Email"),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Container(
+                  constraints: BoxConstraints(
+                    minWidth: 500,
+                    maxWidth: constraints.maxWidth,
+                  ),
+                  child: FractionallySizedBox(
+                    widthFactor: .5,
+                    child: TextField(
+                      controller: passwordController,
+                      textInputAction: TextInputAction.next,
+                      decoration: const InputDecoration(labelText: "Password"),
+                      obscureText: true,
+                    ),
+                  ),
+                ),
+                Container(height: 10),
+                Container(
+                  constraints: BoxConstraints(
+                    minWidth: 500,
+                    maxWidth: constraints.maxWidth,
+                  ),
+                  child: FractionallySizedBox(
+                    widthFactor: .5,
+                    child: SocialLoginButton(
+                      onPressed: () {
+                        _loginWithMicrosoft();
+                        setState(() {});
+                      },
+                      // onPressed: () {},
+                      buttonType: SocialLoginButtonType.generalLogin,
+                      backgroundColor: const Color.fromRGBO(0, 87, 184, 1),
+                    ),
+                  ),
+                ),
+                Container(height: 10),
+                Container(
+                  constraints: BoxConstraints(
+                    minWidth: 500,
+                    maxWidth: constraints.maxWidth,
+                  ),
+                  child: FractionallySizedBox(
+                    widthFactor: .5,
+                    child: SocialLoginButton(
+                      onPressed: () {
+                        _loginWithMicrosoft();
+                        setState(() {});
+                      },
+                      // onPressed: () {},
+                      buttonType: SocialLoginButtonType.microsoft,
+                    ),
+                  ),
+                ),
+                Container(height: 10),
+                Container(
+                  constraints: BoxConstraints(
+                    minWidth: 500,
+                    maxWidth: constraints.maxWidth,
+                  ),
+                  child: FractionallySizedBox(
+                    widthFactor: .5,
+                    child: SocialLoginButton(
+                      onPressed: () => _signUpWithEmail(),
+                      // onPressed: () {},
+                      buttonType: SocialLoginButtonType.generalLogin,
+                      backgroundColor: const Color.fromRGBO(0, 46, 93, 1),
+                      text: "Sign Up",
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                if (errorMessage != "")
+                  Column(
+                    children: [
+                      Text(
+                        errorMessage,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  _signUpWithEmail() async {}
+
+  _loginWithEmail() async {
+    try {
+      final provider = OAuthProvider("microsoft.com");
+      provider.setCustomParameters({
+        // This allows people under BYU org to sign in w/ BYU microsoft accounts
+        "tenant": "common",
+        // This allows people to verify the account they signed in with
+        "prompt": "select_account",
+        // Other types of custom params include:
+        //    {prompt, login}, {prompt, consent}, {login_hint},
+        //    {domain_hint}, and {scope}
+      });
+
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: "jackestes10@yahoo.com",
+        password: "Jellyfish",
+      );
+    } on FirebaseAuthException catch (e) {
+      errorMessage = "${e.code} - ${e.message}";
+      debugPrint(errorMessage);
+    }
+  }
+
   _loginWithMicrosoft() async {
     try {
       final provider = OAuthProvider("microsoft.com");
@@ -107,22 +262,13 @@ class _MicrosoftLoginWidgetState extends State<MicrosoftLoginWidget> {
           // debugPrint(cred.user?.displayName);
           // debugPrint(cred.user?.email);
         } on FirebaseAuthException catch (e) {
-          debugPrint("${e.code} -${e.message}");
+          errorMessage = "${e.code} - ${e.message}";
+          debugPrint(errorMessage);
         }
       }
     } on FirebaseAuthException catch (e) {
-      debugPrint("${e.code} - ${e.message}");
+      errorMessage = "${e.code} - ${e.message}";
+      debugPrint(errorMessage);
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () => _loginWithMicrosoft(),
-        // onPressed: () {},
-        child: const Text('Log in with Microsoft'),
-      ),
-    );
   }
 }
